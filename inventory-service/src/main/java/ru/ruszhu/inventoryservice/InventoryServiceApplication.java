923a6557db5ecf5992150cedd47d2383e06a409e
rus-zhu <microlab982@yandex.ru>
2023-03-15 14:46:23 +0500

    add netflix eureka server and clients

--- inventory-service/src/main/java/ru/ruszhu/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/ru/ruszhu/inventoryservice/InventoryServiceApplication.java
@@ -3,11 +3,13 @@ package ru.ruszhu.inventoryservice;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import ru.ruszhu.inventoryservice.model.Inventory;
 import ru.ruszhu.inventoryservice.repository.InventoryRepository;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class InventoryServiceApplication {
 
 	public static void main(String[] args) {
